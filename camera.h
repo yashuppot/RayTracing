5fcbd1f3abdf749a1f1ff3edf6726f3a80ade600
Yash Uppot <yashuppot@gmail.com>
2024-05-07 14:58:53 -0400

    Change camera settings

--- camera.h
+++ camera.h
@@ -6,9 +6,10 @@
 
 class camera {
     public: 
-        double aspect_ratio = 1.0;
-        int image_width = 100;
-        int samples_per_pixel;
+        double aspect_ratio = 1.0; 
+        int image_width = 100; // Rendered image width
+        int samples_per_pixel; // Number of random samples per pixel
+        int max_depth = 10; // Maximum number of ray bounces
 
 
     void render(const hittable& world) {
@@ -22,7 +23,7 @@ class camera {
                 color pixel_color(0,0,0);
                 for (int sample = 0; sample < samples_per_pixel; sample++) {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(std::cout, pixel_samples_scale * pixel_color);
             }
@@ -75,11 +76,14 @@ class camera {
             return vec3(random_double() - 0.5, random_double() - 0.5, 0);
         }
 
-        color ray_color(const ray& r, const hittable& world) const {
+        color ray_color(const ray& r, int depth, const hittable& world) const {
             hit_record rec;
+            if (depth <= 0) {
+                return color(0,0,0); //after too many bounces, return pure black
+            }
             if (world.hit(r, interval(0, infinity), rec)) {
                 vec3 direction = random_on_hemisphere(rec.normal);
-                return 0.5 * ray_color(ray(rec.p, direction), world);
+                return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
             }
 
             vec3 unit_direction = unit_vector(r.direction());
